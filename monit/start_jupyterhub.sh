2c8b2e1f8707b11a844f372dab4cdaad89bf911f
Pawel Adamczyk <root@1af2a8a8afe9>
2022-09-02 12:59:46 +0200

    2.1.16, poprawka install_ssh, dodanie install_git_2.x

--- monit/start_jupyterhub.sh
+++ monit/start_jupyterhub.sh
@@ -6,9 +6,10 @@ conda activate jupyter
 
 mkdir -p $JUPYTERHUB_WORKDIR
 cd $JUPYTERHUB_WORKDIR
-JUPYTHER_OPT=""
+JUPYTHER_OPT="--pid-file=/var/run/jupyterhub.pid"
 if ! [[ -z "$DEBUG" ]]; then
   JUPYTHER_OPT="$JUPYTHER_OPT --debug"
 fi
 jupyterhub --config $JUPYTERHUB_CONFIGDIR/jupyterhub_config.py $JUPYTHER_OPT &>> /var/log/jupyterhub.log&
+#jupyterhub --config $JUPYTERHUB_CONFIGDIR/jupyterhub_config.py $JUPYTHER_OPT
 exit 0
